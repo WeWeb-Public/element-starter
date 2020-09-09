610eb2ee2b86ca287322cf3aa99aff457b9701de
fbriou <fbriou@gmail.com>
2020-09-09 11:32:11 +0200

    init

---
+++ src/wwObjectButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <p class="transition_rouge">
+        <a href target="_blank">My button</a>
+    </p>
+</template>
+
+<script>
+export default {
+    name: "__COMPONENT_NAME__",
+    props: {
+        wwObjectCtrl: Object,
+        wwAttrs: {
+            type: Object,
+            default: {},
+        },
+    },
+    data() {
+        return {
+        };
+    },
+    computed: {
+        wwObject() {
+            return this.wwObjectCtrl.get();
+        },
+    },
+    methods: {
+        init() { },
+    },
+    mounted() {
+        this.init();
+
+        this.$emit("ww-loaded", this);
+    },
+    beforeDestroy() { },
+};
+</script>
+
+<style scoped>
+</style>
